7a89a9b5a0c44a678868a41f03e5b27934e565f0
Varlygin Dmitry <varligin@mail.ru>
2019-05-29 22:56:46 +0700

    add for registration and login

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,18 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
+import 'vuetify/dist/vuetify.min.css';
+
+
+import login from './components/autch/LoginComponent.vue';
+import registration from './components/autch/RegistrationComponent.vue';
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -20,13 +32,44 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
-const app = new Vue({
+const routes = [
+    {path: '/login', component: login, name: 'login'},
+    {path: '/registration', component: registration, name: 'registration'},
+   ];
+   //const router = new VueRouter({ routes });
+ 
+   //const app = new Vue({ router }).$mount('#app');
+   /*const app = new Vue({
     el: '#app',
-});
+});*/
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+// 2. Определяем несколько маршрутов
+// Каждый маршрут должен указывать на компонент.
+// "Компонентом" может быть как конструктор компонента, созданный
+// через `Vue.extend()`, так и просто объект с опциями компонента.
+// Мы поговорим о вложенных маршрутах позднее.
+/*const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]*/
+
+// 3. Создаём экземпляр маршрутизатора и передаём маршруты в опции `routes`
+// Вы можете передавать и дополнительные опции, но пока не будем усложнять.
+const router = new VueRouter({
+  routes // сокращённая запись для `routes: routes`
+})
+
+// 4. Создаём и монтируем корневой экземпляр приложения.
+// Убедитесь, что передали экземпляр маршрутизатора в опции
+// `router`, чтобы позволить приложению знать о его наличии.
+const app = new Vue({
+  router
+}).$mount('#app')
+
---
+++ resources/js/components/autch/LoginComponent.vue
@@ -0,0 +1,48 @@
+<template>
+   <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Форма входа</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip bottom>
+                  <v-btn
+                    icon
+                    large
+                    :href="source"
+                    target="_blank"
+                    slot="activator"
+                  >
+                    <v-icon large>code</v-icon>
+                  </v-btn>
+                  <span>Source</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field prepend-icon="person" name="email" label="email" type="text"></v-text-field>
+                  <v-text-field prepend-icon="lock" name="password" label="Пароль" id="password" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Войти</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
