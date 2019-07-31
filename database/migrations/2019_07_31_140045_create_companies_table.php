936eb9b74dd25d6e845d3ecf9d9724a726c3d168
Varlygin Dmitry <varligin@mail.ru>
2019-07-31 22:31:02 +0700

    create migration and seed for company

---
+++ database/migrations/2019_07_31_140045_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100)->nullable();
+            $table->text('description')>nullable();
+            $table->string('logo', 100)->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+             });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $adminRole = DB::table('roles')->where('name', '=', 'Администратор')->first();
+        if (!$adminRole) { 
+            DB::table('roles')->insert(['name'=>'Администратор']);
+        }
+    }
+}
